7ec4c6ee47819092562a29fb3ba86dcbf8d75bf1
Kaur Pääro <kaur@helmes.ee>
2016-08-17 14:22:25 +0300

    clean error_log

--- api/v1/classes/piletilevi.php
+++ api/v1/classes/piletilevi.php
@@ -32,7 +32,7 @@ class PiletileviApi {
 		$url .= $query;
 
 		$uri = $this->getBasePath().$url;
-		error_log($uri);
+		
 		$response = \Httpful\Request::getQuick($uri);
 		return  json_decode($response->__toString());
 	}
