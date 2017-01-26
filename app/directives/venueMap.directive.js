20d01b255b89966bfe9e17d592e6396acb7fff7d
Viktoria <viktoria@inslab.net>
2017-01-26 15:07:32 +0200

    Seats map: local domain change

--- app/directives/venueMap.directive.js
+++ app/directives/venueMap.directive.js
@@ -16,7 +16,7 @@
 
         piletilevi.venuemap = {
           SHOP_DOMAIN: 'shop.piletilevi.ee',
-          ASSETS_DOMAIN: 'localhost:83/boui',
+          ASSETS_DOMAIN: 'localhost',
           // ASSETS_DOMAIN: 'bo2.piletilevi.ee',
           DEFAULT_SEAT_HOVER_COLOR: '#27272e',
           DEFAULT_SEAT_ACTIVE_COLOR: '#27272e',
