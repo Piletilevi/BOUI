d69ac09a73748174e4eaa4e0762fe2f025046319
aw-dmitrishapoval <dmitri.shapoval@artweb.ee>
2017-02-17 16:25:23 +0200

    0003879: Design: Choosing land center or sales center from the header should make a request

--- app/directives/ngConfirmClick.directive.js
+++ app/directives/ngConfirmClick.directive.js
@@ -1,21 +1,61 @@
-(function () {
-  'use strict';
-  angular.module('boApp')
-    .directive('ngConfirmClick', ngConfirmClick);
+(function() {
+	'use strict';
+	angular.module('boApp')
+		.directive('ngConfirmClick', ngConfirmClick);
 
-  function ngConfirmClick() {
-    return {
-      priority: -1,
-      restrict: 'A',
-      link: function(scope, element, attrs){
-        element.bind('click', function(e){
-          var message = attrs.ngConfirmClick;
-          if(message && !confirm(message)){
-            e.stopImmediatePropagation();
-            e.preventDefault();
-          }
-        });
-      }
-    }
-  }
+	function ngConfirmClick() {
+		return {
+			priority: -1,
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+
+				var confirmModal;
+
+				element.bind('click', function(e) {
+					var message = attrs.ngConfirmClick;
+					var description = attrs.ngConfirmDescription;
+					if (message) {
+						e.stopImmediatePropagation();
+						e.preventDefault();
+						$('.bo-confirm-modal').remove();
+						var modalHtml = '<div class="modal fade bo-modal bo-confirm-modal">' +
+							'<div class="modal-dialog">' +
+							'	<div class="modal-content">' +
+							'		<div class="modal-header">' +
+							'			<button type="button" class="btn btn-sm btn-default bo-close-btn bo-confirm-modal-close"><i class="fa fa-times"></i></button>' +
+							'			<h4 class="modal-title">' + message + '</h4>' +
+							'		</div>' +
+							'		<div class="modal-body">' + description +
+							'		</div>' +
+							'		<div class="modal-footer">' +
+							'			<button type="button" class="btn btn-success bo-confirm-modal-submit">Send</button>' +
+							'			<button type="button" class="btn btn-default bo-confirm-modal-close">Cancel</button>' +
+							'		</div>' +
+							'	</div>' +
+							'</div>' +
+						'</div>';
+						$(modalHtml).appendTo('body');
+						confirmModal = $('.bo-confirm-modal');
+						confirmModal.modal('show');
+						$('.dropdown.open .dropdown-toggle').dropdown('toggle');
+						confirmModal.find('.bo-confirm-modal-close').bind('click', function() {
+							closeModal();
+						});
+						confirmModal.find('.bo-confirm-modal-submit').bind('click', function() {
+							locateAction();
+						});
+					}
+				});
+
+				function locateAction() {
+					scope.$eval(attrs.ngClick);
+					closeModal();
+				}
+
+				function closeModal() {
+					confirmModal.modal('hide');
+				}
+			}
+		}
+	}
 })();
