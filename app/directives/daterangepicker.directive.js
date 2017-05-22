f6301be1e150220ec67e7556710b28efe974aab8
aw-dmitrishapoval <dmitri.shapoval@artweb.ee>
2017-05-22 07:50:59 +0300

    design changes

--- app/directives/daterangepicker.directive.js
+++ app/directives/daterangepicker.directive.js
@@ -109,7 +109,7 @@
 					options.template +=
 						'<div class="range_inputs">' +
 						'<button class="applyBtn" disabled="disabled" type="button"></button> ' +
-						'<button class="btn resetBtn" type="button">' + $translate.instant('api_calendar_resetLabel') + '</button>' +
+						'<button class="btn btn-default resetBtn" type="button">' + $translate.instant('api_calendar_resetLabel') + '</button>' +
 						'<div class="clearer"></div>' +
 						'</div>' +
 						'</div>' +
