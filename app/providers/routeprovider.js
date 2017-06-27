34fca1a12366610856a6326a7fa9ec8aac7b8e48
aw-dmitrishapoval <dmitri.shapoval@artweb.ee>
2017-06-27 12:03:21 +0300

    password change page design

--- app/providers/routeprovider.js
+++ app/providers/routeprovider.js
@@ -79,6 +79,7 @@
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
             $rootScope.$log = $log;
             authService.checkUserAuth(next);
+            $rootScope.filterNotNeeded = ($location.path() == '/changepassword');
         });
 
         $rootScope.$on('$routeChangeSuccess', function(){
