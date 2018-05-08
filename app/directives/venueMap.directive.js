5033889ad9503e65b46924145335b851a4ad7386
HGRUPP\jaan.bachmann <jaan.bachmann@helmes.ee>
2018-05-08 15:37:00 +0300

    Adding debug message

--- app/directives/venueMap.directive.js
+++ app/directives/venueMap.directive.js
@@ -92,6 +92,7 @@ function VenueMapDirective($parse, $location, $translate, eventService) {
                             sectorData = sector;
                         }
                     });
+                    console.log($scope.reservationMode);
                     if (!$scope.reservationMode || sectorData.statistics.availableTickets != 0) {
                         $scope.setSelectedSectionId(sectionId);
                         $scope.$apply();
